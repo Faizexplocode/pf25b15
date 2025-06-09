9410a9085d1c705a401190ec65e609450626a4eb
Maul139 <maulanabahri18@gmail.com>
2025-06-09 16:01:38 +0700

    Delete TTTConsuleNewOO.java

--- Seed.java
+++ Seed.java
@@ -1,25 +1,12 @@
 /**
- * This enum is used by:
- * 1. Player: takes value of CROSS or NOUGHT
- * 2. Cell content: takes value of CROSS, NOUGHT, or NO_SEED.
- *
- * We also attach a display icon (text or image) for each of the item,
- *   and define the related variable/constructor/getter.
- *
- * Ideally, we should define two enums with inheritance, which is,
- *  however, not supported.
- */
-public enum Seed {   // to save as "Seed.java"
-    CROSS("X"), NOUGHT("O"), NO_SEED(" ");
+ * Enum Seed digunakan oleh:
+ * 1. Player: menggunakan nilai CROSS atau NOUGHT
+ * 2. Cell content: menggunakan nilai CROSS, NOUGHT, atau NO_SEED
 
-    // Private variable
-    private String icon;
-    // Constructor (must be private)
-    private Seed(String icon) {
-        this.icon = icon;
-    }
-    // Public Getter
-    public String getIcon() {
-        return icon;
-    }
+ * Idealnya, kita ingin membuat dua enum dengan pewarisan, tetapi ini tidak didukung di Java.
+ */
+public enum Seed {
+    CROSS,
+    NOUGHT,
+    NO_SEED
 }
